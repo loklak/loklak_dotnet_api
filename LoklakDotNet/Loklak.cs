8c8de196ff73618dccf6e2b4190b0c4bc94b91d6
aneeshd16 <aneeshdevasthale@gmail.com>
2016-02-10 00:51:02 +0530

    Initial

---
+++ LoklakDotNet/Loklak.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoklakDotNet
+{
+    public class Loklak
+    {
+        private string apiUrl;
+        private HttpClient client;
+
+        private async Task<string> ProcessUrlAsync(string method, string parameters)
+        {
+            var uriString = apiUrl + method;
+            if(parameters!=null)
+            {
+                uriString += "?" + parameters;
+            }
+            
+            var result = await client.GetStringAsync(new Uri(uriString, UriKind.Absolute));
+            System.Diagnostics.Debug.WriteLine(result);
+            return result;
+        }
+
+        /// <summary>
+        /// Initializes the Loklak API wrapper
+        /// </summary>
+        /// <param name="apiUrl">Optional API Url</param>
+        public Loklak(String apiUrl = "http://loklak.org/api/")
+        {
+            this.apiUrl = apiUrl;
+            client = new HttpClient();
+        }
+
+        /// <summary>
+        /// The status servlet shows the size of the internal Elasticsearch search index for messages and users. Furthermore, the servlet reflects the current browser clients settings in the client_info.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<string> status()
+        {
+            return (await ProcessUrlAsync("status.json", null));
+        }
+
+        /// <summary>
+        /// The hello servlet is part of the loklak peer-to-peer bootstrap process and shall be used to announce that a new client has been started up. The hello request is done automatically after a loklak startup against the loklak backend as configured in the settings in field backend. The back-end server then does not return any data, just an 'ok' string object.
+        /// </summary>
+        /// <returns>{"status":"ok"}</returns>
+        public async Task<string> hello()
+        {
+            return (await ProcessUrlAsync("hello.json", null));
+        }
+
+        /// <summary>
+        /// This servlet combined the result of the hello calls from all peers and provides a list of addresses where the remote peers can be accessed.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<string> peers()
+        {
+            return (await ProcessUrlAsync("peers.json", null));
+        }
+
+        /// <summary>
+        /// This servlet provides geocoding of place names to location coordinates and also reverse geocoding of location coordinates to place names.
+        /// </summary>
+        /// <param name="places">List of place names</param>
+        /// <returns></returns>
+        public async Task<string> geocode(IList<string> places)
+        {
+            var qs = "places=" + string.Join(",", places);
+            return (await ProcessUrlAsync("geocode.json", qs));
+        }
+
+        /// <summary>
+        /// This servlet provides the retrieval of user followers and the accounts which the user is following.
+        /// </summary>
+        /// <param name="screen_name">The screen name of the Twitter user without "@"</param>
+        /// <param name="follower_count">The maximum number of follower profiles to be fetched</param>
+        /// <param name="following_count">The maximum number of following profiles to be fetched</param>
+        /// <returns></returns>
+        public async Task<string> user(string screen_name, int follower_count=0, int following_count=0)
+        {
+            var qs = "screen_name=" + screen_name + "&followers=" + follower_count.ToString() + "&following=" + following_count.ToString();
+            System.Diagnostics.Debug.WriteLine(qs);
+            return (await ProcessUrlAsync("user.json", qs));
+        }
+
+        public async Task<string> search(SearchTerm q, int count = 100, string source="cache", IList<string> fields = null, int limit=-1, int timeZoneOffset=-1)
+        {
+            var qs = "q=" + q.getQueryString() + "&count=" + count.ToString() + "&source=" + source;
+            if(fields!= null)
+            {
+                qs += "&fields=" + string.Join(",", fields);
+            }
+            if(limit!=-1)
+            {
+                qs += "&limit=" + limit;
+            }
+            if(timeZoneOffset!=-1)
+            {
+                qs += "&timeZoneOffset=" + timeZoneOffset;
+            }
+            System.Diagnostics.Debug.WriteLine(qs);
+            return (await ProcessUrlAsync("search.json", qs));
+        }
+    }
+
+    public class SearchTerm
+    {
+        public string terms = "";
+        public string from = "";
+        public string near = "";
+        public DateTime since;
+        public DateTime until;
+        
+        public string getQueryString()
+        {
+            var qs = terms;
+            if(!string.IsNullOrWhiteSpace(from))
+            {
+                qs += " from:" + from;
+            }
+            if (!string.IsNullOrWhiteSpace(near))
+            {
+                qs += " near:" + near;
+            }
+            if(since.Ticks>0)
+            {
+                qs += " since:" + since.ToString("yyyy-MM-dd");
+            }
+            if (until.Ticks > 0)
+            {
+                qs += " until:" + until.ToString("yyyy-MM-dd");
+            }
+            return qs;
+        }
+    }
+}
---
+++ LoklakDotNetTests/UnitTest1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LoklakDotNet;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace LoklakDotNetTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async Task status()
+        {
+            Loklak loklak = new Loklak();
+            var result = await loklak.status();
+            var d = JObject.Parse(result);
+            Assert.IsNotNull(d.Property("system"));
+        }
+
+        [TestMethod]
+        public async Task hello()
+        {
+            Loklak loklak = new Loklak();
+            var result = await loklak.hello();
+            var d = JObject.Parse(result);
+            Assert.IsNotNull(d.Property("status"));
+        }
+
+        [TestMethod]
+        public async Task peers()
+        {
+            Loklak loklak = new Loklak();
+            var result = await loklak.peers();
+            var d = JObject.Parse(result);
+            Assert.IsNotNull(d.Property("peers"));
+            Assert.IsTrue(((JArray)d.GetValue("peers")).Count >= 0);
+        }
+
+        [TestMethod]
+        public async Task geocode()
+        {
+            Loklak loklak = new Loklak();
+            var p = new List<string>();
+            p.Add("Delhi");
+            p.Add("Berlin");
+            var result = await loklak.geocode(p);
+            var d = JObject.Parse(result);
+            Assert.IsNotNull(d.Property("locations"));
+            Assert.AreEqual(((JObject)(((JObject)d.GetValue("locations")).GetValue("Delhi"))).GetValue("country_code").ToString(),"IN");
+            Assert.AreEqual(((JObject)(((JObject)d.GetValue("locations")).GetValue("Berlin"))).GetValue("country_code").ToString(), "DE");
+        }
+
+        [TestMethod]
+        public async Task user()
+        {
+            Loklak loklak = new Loklak();
+            var result = await loklak.user("loklak_app", 5, 5);
+            var d = JObject.Parse(result);
+            Assert.IsNotNull(d.Property("user"));
+            Assert.AreEqual(((JObject)(((JObject)d.GetValue("user")))).GetValue("id").ToString(), "3090229939");
+            Assert.IsNotNull(d.Property("topology"));
+            Assert.IsTrue(((JArray)(((JObject)(d.GetValue("topology"))).GetValue("following"))).Count >= 5);
+            Assert.IsTrue(((JArray)(((JObject)(d.GetValue("topology"))).GetValue("followers"))).Count >= 5);
+        }
+
+        [TestMethod]
+        public async Task search()
+        {
+            Loklak loklak = new Loklak();
+            var st = new SearchTerm();
+            st.terms = "loklak";
+            st.since = DateTime.Now;
+            st.until = DateTime.Now;
+            st.since.AddMonths(-2);
+            var result = await loklak.search(st);
+            var d = JObject.Parse(result);
+            Assert.IsNotNull(d.Property("search_metadata"));
+            Assert.IsTrue(((JArray)d.GetValue("statuses")).Count > 0);
+
+        }
+    }
+}
